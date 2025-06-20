c3dc80a5fa15415fef2edc69d56c52a3e7072c06
raghuprasadks <prasadraghuks@gmail.com>
2025-06-20 21:52:33 +0530

    updated

--- programs/fullstack/huggingfacehub/hostedmistralapi.py
+++ programs/fullstack/huggingfacehub/hostedmistralapi.py
@@ -3,21 +3,8 @@ from huggingface_hub import InferenceClient
 
 client = InferenceClient(
     provider="nebius",
-    api_key="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",
+    api_key="your huggingface api key here"  # Replace with your actual API key,
 )
-"""
-completion = client.chat.completions.create(
-    model="mistralai/Mistral-Nemo-Instruct-2407",
-    messages=[
-        {
-            "role": "user",
-            "content": "What is the capital of France?"
-        }
-    ],
-)
-print(completion.choices[0].message)
-"""
-
 
 
 def get_response(prompt):
